3aa814fadfeb21dfb300bbde04c86c8129d8ef90
Marc <mdreardon@wpi.edu>
2018-09-06 10:42:26 -0400

    Marc

--- server.js
+++ server.js
@@ -5,40 +5,69 @@ var http = require('http')
 
 
 // NOTE: your dataset can be as simple as the following, you need only implement functions for addition, deletion, and modification that are triggered by outside (i.e. client) actions, and made available to the front-end
-var data = [
-  {'model': 'toyota', 'year': 1999, 'mpg': 23},
-  {'model': 'honda', 'year': 2004, 'mpg': 30},
-  {'model': 'ford', 'year': 1987, 'mpg': 14}
+var books = [
+  {'name': 'Dave', 'email': 'hi2@gmail.com'},
+  {'name': 'Ben', 'email': 'brv@gmail.com'},
+  {'name': 'Melissa', 'email': 'league@gmail.com'}
 ]
 
 var server = http.createServer (function (req, res) {
-  var uri = url.parse(req.url)
 
-  switch( uri.pathname ) {
-    case '/':
-      sendFile(res, 'public/index.html')
-      break
-    case '/index.html':
-      sendFile(res, 'public/index.html')
-      break
-    case '/css/style.css':
-      sendFile(res, 'public/css/style.css', 'text/css')
-      break
-    case '/js/scripts.js':
-      sendFile(res, 'public/js/scripts.js', 'text/javascript')
-      break
-    case '/css/horoscope_bg.jpg':
-      sendFile(res, 'public/css/horoscope_bg.jpg')
-      break
-    case '/css/caveat-regular.ttf':
-      sendFile(res, 'public/css/caveat-regular.ttf')
-      break
-    case '/submit.html':
-      sendFile(res, 'public/submit.html')
-      break
-    default:
-      res.end('404 not found')
+  if (req.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        req.on('data', function (data) {
+            body += data;
+            console.log("Partial body: " + body);
+        });
+        req.on('end', function () {
+            console.log("Body: " + body);
+
+            // add name and email to the data set
+            var i;
+            var newbook;
+            var all = body.split("=");
+            for (i = 1; i < all.length; i++) {
+                var indiv = all[i].split("&");
+                console.log(indiv[0]);
+            }
+
+        });
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('post received');
+
+  } else {
+
+    var uri = url.parse(req.url)
+
+    switch( uri.pathname ) {
+      case '/':
+        sendFile(res, 'public/index.html')
+        break
+      case '/index.html':
+        sendFile(res, 'public/index.html')
+        break
+      case '/css/style.css':
+        sendFile(res, 'public/css/style.css', 'text/css')
+        break
+      case '/js/scripts.js':
+        sendFile(res, 'public/js/scripts.js', 'text/javascript')
+        break
+      case '/css/horoscope_bg.jpg':
+        sendFile(res, 'public/css/horoscope_bg.jpg')
+        break
+      case '/css/caveat-regular.ttf':
+        sendFile(res, 'public/css/caveat-regular.ttf')
+        break
+      case '/submit.html':
+        sendFile(res, 'public/submit.html')
+        break
+      default:
+        res.end('404 not found')
+    }
+
   }
+
 })
 
 server.listen(process.env.PORT || port);
