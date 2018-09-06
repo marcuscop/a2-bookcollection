33ba225f36ef0075418bd171a4dc892151777936
sushisoju <hadieutrang@gmail.com>
2018-09-06 11:27:17 -0400

    Changed to book collection. Made table

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,13 @@
 // Add some Javascript code here, to run on the front end.
-
-console.log("Welcome to assignment 2!")
+function addToTable() {
+    var x = document.getElementById("form1");
+    var text = "";
+    var i;
+    var rowlen = document.getElementById("myTable").rows.length;
+    for (i = 0; i < x.length ;i++) {
+        text += x.elements[i].value + "<br>";
+        document.getElementById("myTable").insertRow();
+        document.getElementById("myTable").rows.item(rowlen+1).innerHTML = text;
+    }
+    document.getElementById("demo").innerHTML = text;
+}
